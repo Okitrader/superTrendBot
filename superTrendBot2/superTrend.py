2e17ae856f195d055a2a66d8f8665cac1f754623
Akio <ado.okinawa@gmail.com>
2021-12-05 08:42:50 +0900

    End of Supertrend video 3. Next will add buy and sell signals

--- superTrendBot2/superTrend.py
+++ superTrendBot2/superTrend.py
@@ -2,7 +2,7 @@ import ccxt
 import pandas as pd
 import numpy as np
 from datetime import datetime
-pd.set_option('display.max_rows', None)
+#pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
 exchange = ccxt.binance()
@@ -29,20 +29,25 @@ def atr(data, period=14):
 def supertrend(df, period=7, multiplier=3):
     hl2 = (df['high'] + df['low']) / 2
     df['atr'] = atr(df, period)
-    df['basic_upper_band'] = hl2 + (multiplier * df['atr'])
-    df['basic_lower_band'] = hl2 - (multiplier * df['atr'])
+    df['upper_band'] = hl2 + (multiplier * df['atr'])
+    df['lower_band'] = hl2 - (multiplier * df['atr'])
     df['in_uptrend'] = True
 
-    for current in range(1, len(df.index)):
-        prev = current - 1
+    for current in range(1, len(df.index)): # creates a current variable that iterates over the index of the dataframe
+        previous = current - 1 # previous is the current minus the last value
 
-        if df.iloc[current]['close'] > df.iloc[prev]['basic_upper_band']: # if current close is above upper band
+        if df.iloc[current]['close'] > df.iloc[previous]['upper_band']: # if current close is above upper band
             df['in_uptrend'].iloc[current] = True # set in_uptrend to True
-        elif df.iloc[current]['close'] < df.iloc[prev]['basic_lower_band']: # if current close is below lower band
+        elif df.iloc[current]['close'] < df.iloc[previous]['lower_band']: # if current close is below lower band
             df['in_uptrend'].iloc[current] = False # set in_uptrend to False
         else:
-            df['in_uptrend'].iloc[current] = df['in_uptrend'].iloc[prev] # the trend is the same as the previous trend
+            df['in_uptrend'].iloc[current] = df['in_uptrend'].iloc[previous] # the trend is the same as the previous trend
 
+            if df['in_uptrend'][current] and df['lower_band'][current] < df['lower_band'][previous]: # if in uptrend and lower band is below previous lower band
+                df['lower_band'].iloc[current] = df['lower_band'].iloc[previous] # set lower band to previous lower band
+
+            if not df['in_uptrend'][current] and df['upper_band'][current] > df['upper_band'][previous]: # if in downtrend and upper band is above previous upper band
+                df['upper_band'].iloc[current] = df['upper_band'].iloc[previous] # set upper band to previous upper band
 
 
     print(df)
